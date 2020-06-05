d48cc6b11dbbe20e3d0819b0423ba26eeddc1cd7
NickPaladino <66493184+NickPaladino@users.noreply.github.com>
2020-06-05 15:34:47 -0500

    Adding Nick's Local Files

---
+++ The Cube/Assets/Nick_Import/Track_Manager_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track_Manager_Script : MonoBehaviour
+{
+     GameObject[] Platforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Platforms = GameObject.FindGameObjectsWithTag("Platform");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ActivatePlatforms();
+        }
+    }
+
+    void ActivatePlatforms()
+    {
+        Platforms = GameObject.FindGameObjectsWithTag("Platform");
+
+        foreach (GameObject platform in Platforms)
+        {
+            platform.GetComponent<Track_Platform_Script>().ActivatePlatformMovement();
+        }
+    }
+}
---
+++ The Cube/Assets/Nick_Import/Track_Platform_Script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track_Platform_Script : MonoBehaviour
+{
+    bool IsMoving = false;
+    bool IsDestroyed = false;
+    float RandomStartDealy = 0;
+
+    private Vector3 targetScale;
+    float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetScale = new Vector3(0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsMoving == true && IsDestroyed != true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 5);
+        }
+
+        if(IsDestroyed == true)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
+            
+            if(transform.localScale == targetScale)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void ActivatePlatformMovement()
+    {
+        StartCoroutine(PlatformMoveTimer());
+    }
+
+    IEnumerator PlatformMoveTimer()
+    {
+
+        RandomStartDealy = Random.Range(0.25f, 0.65f);
+        yield return new WaitForSeconds(RandomStartDealy);
+        IsMoving = true;
+        yield return new WaitForSeconds(1.5f);
+        IsMoving = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PlatformBreak")
+        {
+            IsDestroyed = true;
+            Debug.Log("IsDestoryed");
+        }
+    }
+}
