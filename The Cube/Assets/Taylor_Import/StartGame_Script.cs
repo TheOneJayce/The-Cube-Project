dbfd04c062a173bff1483f5bd9b678183033c7fb
TJMcKinnon <39864919+TJMcKinnon@users.noreply.github.com>
2020-06-05 14:45:13 -0700

    Adding Start Menu
    Created StartMenu Scene
    Created StartMenu_Script
    Added StartGameButton and functionality
    Added SettingsButton and functionality
    Added HelpButton and functionality
    Added CreditsButton and functionality

---
+++ The Cube/Assets/Taylor_Import/StartGame_Script.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame_Script : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("SceneName"); 
+
+        //Delete when scene is ready
+        Debug.Log("Game has started");
+    }
+}
