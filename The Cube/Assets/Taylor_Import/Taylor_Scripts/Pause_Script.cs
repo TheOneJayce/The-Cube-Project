1e2d99cf35d95ba1809bc87c4d54b942dd368d34
TJMcKinnon <39864919+TJMcKinnon@users.noreply.github.com>
2020-06-06 08:58:13 -0700

    Added User UI and Organized Taylor_Import
    Added Pause Menu UI in Taylor_Scene
    Added Timer_Canvas in Taylor_Scene
    Added Coin_Canvas in Taylor_Scene
    Created LoseCanvas Prefab
    Created PauseMenuCanvas Prefab
    Created WinCanvas Prefab
    Created Taylor_Prefabs folder in Taylor_Import
    Added all of Taylor_Scene prefabs too Taylor_Prefabs
    Created Taylor_Scripts folder in Taylor_Import
    Added all of the Taylor_Scene scripts too Taylor_Scripts

---
+++ The Cube/Assets/Taylor_Import/Taylor_Scripts/Pause_Script.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause_Script : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    [Header("General References")]
+    public GameObject pauseMenu;
+
+    [Header("Resume Time")]
+    public Text resumeTime;
+    public float resumeTimeLeft;
+    public GameObject resumeTimeGO;
+    public GameObject resumeTimeTextGO;
+    public bool gameIsResuming = false;
+
+    [Header("Timer")]
+    public GameObject TimerGO;
+    public GameObject TimerTextGO;
+
+    [Header("Track_Manager_Script Reference")]
+    public GameObject trackManager;
+    public Track_Manager_Script trackManagerScript;
+
+    public void Start()
+    {
+        trackManagerScript = trackManager.GetComponent<Track_Manager_Script>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            pauseMenu.SetActive(true);
+            PauseGame();
+        }
+
+        if(gameIsResuming == true)
+        {
+            TimerGO.SetActive(false);
+            TimerTextGO.SetActive(false);
+
+            resumeTimeTextGO.SetActive(true);
+            resumeTimeGO.SetActive(true);
+
+            resumeTimeLeft -= Time.unscaledDeltaTime;
+            resumeTime.text = resumeTimeLeft.ToString("0");
+
+            if (resumeTimeLeft <= 0)
+            {           
+                Time.timeScale = 1;
+                gameIsResuming = false;
+
+                resumeTimeGO.SetActive(false);
+                resumeTimeTextGO.SetActive(false);
+
+                TimerTextGO.SetActive(true);
+                TimerGO.SetActive(true);
+
+                resumeTimeLeft = 3;
+
+                gameIsPaused = false;
+            }
+        }
+
+    }
+
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        gameIsResuming = true;
+    }
+}
