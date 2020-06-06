4d2ebb7a5a685898bd0e2fb4943ff6cb766ae513
TheOneJayce <47903533+TheOneJayce@users.noreply.github.com>
2020-06-06 12:18:04 -0400

    Basic Movment
    The player is able to move Forward, Back, Left and Right.

---
+++ The Cube/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 15f;
+    private Vector3 inputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y, Input.GetAxisRaw("Vertical") * speed);
+
+
+        rb.velocity = inputVector;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
