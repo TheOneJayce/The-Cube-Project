078595b92f9d435e554b020bcc041f9d1f55d547
TJMcKinnon <39864919+TJMcKinnon@users.noreply.github.com>
2020-06-09 15:26:35 -0700

    Art and Win/Lose scenarios
    Added character and ground art assets
    Added lose scenario for player collision with obstacles
    Added lose scenario for player falling off the level
    Added win scenario for player reaching the end
    Added functioning reset button for Taylor_Scene

---
+++ The Cube/Assets/Taylor_Import/Taylor_Scripts/PlayerCollision_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision_Script : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public GameObject loseMenu;
+    public GameObject winMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If player falls below Y -10, player loses
+        if(player.gameObject.transform.position.y <= -10)
+        {
+            loseMenu.SetActive(true);
+            player.SetActive(false);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //If player collides with obstacle, they lose
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            loseMenu.SetActive(true);
+            player.SetActive(false);
+        }
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "WinTrigger")
+        {
+            winMenu.SetActive(true);
+            player.SetActive(false);
+        }
+    }
+}
--- The Cube/Assets/Taylor_Import/Taylor_Scripts/Restart_Script.cs
+++ The Cube/Assets/Taylor_Import/Taylor_Scripts/Restart_Script.cs
@@ -6,8 +6,8 @@ public class Restart_Script : MonoBehaviour
     public void RestartLevel()
     {
         // This should work properly once the scenes are in the build settings
-        /*Scene loadedLevel = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(loadedLevel.buildIndex);*/
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex);
 
         //Delete when scene is ready
         Debug.Log("Player has restarted their current level");
